f549a2b0a2cb15840bb24622eb49feb9509c1e33
Demasu <kc9guu@gmail.com>
2018-09-04 17:27:28 -0500

    Simplify boolean expression in SummonCrab

--- app/src/main/java/com/demasu/testpixeldungeon/actors/skills/SummonCrab.java
+++ app/src/main/java/com/demasu/testpixeldungeon/actors/skills/SummonCrab.java
@@ -70,7 +70,7 @@ public class SummonCrab extends ActiveSkill2 {
                 }
             }
 
-            if ( spawned == true ) {
+            if ( spawned ) {
                 hero.MP -= getManaCost();
                 StatusPane.manaDropping += getManaCost();
                 castTextYell();
