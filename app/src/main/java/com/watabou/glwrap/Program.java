90fd2f970b6cd23cd7fca0d161482e0e6a1ef3ab
Demasu <kc9guu@gmail.com>
2018-09-24 19:09:24 -0500

    Converted for loop to foreach in Program

--- app/src/main/java/com/watabou/glwrap/Program.java
+++ app/src/main/java/com/watabou/glwrap/Program.java
@@ -29,8 +29,8 @@ public class Program {
 
     public static Program create ( Shader... shaders ) {
         Program program = new Program();
-        for ( int i = 0; i < shaders.length; i++ ) {
-            program.attach( shaders[i] );
+        for ( Shader shader : shaders ) {
+            program.attach( shader );
         }
         program.link();
         return program;
