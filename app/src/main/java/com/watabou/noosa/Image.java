02e1f1ac788b4fd829ab746b9de86c31a9ee8c07
Demasu <kc9guu@gmail.com>
2018-10-04 18:49:30 -0500

    Suppress warning about copy constructor in Image, since I have no idea where it is getting the idea about the number of fields being wrong

--- app/src/main/java/com/watabou/noosa/Image.java
+++ app/src/main/java/com/watabou/noosa/Image.java
@@ -43,6 +43,7 @@ public class Image extends Visual {
         verticesBuffer = Quad.create();
     }
 
+    @SuppressWarnings ( "CopyConstructorMissesField" )
     public Image ( Image src ) {
         this();
         copy( src );
