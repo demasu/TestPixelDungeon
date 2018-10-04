26b525b1f4ca4bdac890bc782b76cebf3caa5c7c
Demasu <kc9guu@gmail.com>
2018-10-04 18:21:24 -0500

    Changed method to void since the return is never used, in Group

--- app/src/main/java/com/watabou/noosa/Group.java
+++ app/src/main/java/com/watabou/noosa/Group.java
@@ -259,13 +259,10 @@ public class Group extends Gizmo {
         }
     }
 
-    public Gizmo sendToBack ( Gizmo g ) {
+    public void sendToBack ( Gizmo g ) {
         if ( members.contains( g ) ) {
             members.remove( g );
             members.add( 0, g );
-            return g;
-        } else {
-            return null;
         }
     }
 }
