b29b45e0beb8f53e8d67159ddfb149ff96483398
Demasu <kc9guu@gmail.com>
2018-10-10 18:26:25 -0500

    Split up multiple variable assignment in Rect

--- app/src/main/java/com/watabou/utils/Rect.java
+++ app/src/main/java/com/watabou/utils/Rect.java
@@ -68,7 +68,10 @@ public class Rect {
     }
 
     public Rect setEmpty () {
-        left = right = top = bottom = 0;
+        left = 0;
+        right = 0;
+        top = 0;
+        bottom = 0;
         return this;
     }
 
