f68e1885408309956926d424c53a67adf16c98e0
Demasu <kc9guu@gmail.com>
2018-10-10 18:19:19 -0500

    Removed unused method in Random

--- app/src/main/java/com/watabou/utils/Random.java
+++ app/src/main/java/com/watabou/utils/Random.java
@@ -133,18 +133,4 @@ public class Random {
         }
     }
 
-    public static <U, V> void shuffle ( U[] u, V[] v ) {
-        for ( int i = 0; i < u.length - 1; i++ ) {
-            int j = Int( i, u.length );
-            if ( j != i ) {
-                U ut = u[i];
-                u[i] = u[j];
-                u[j] = ut;
-
-                V vt = v[i];
-                v[i] = v[j];
-                v[j] = vt;
-            }
-        }
-    }
 }
